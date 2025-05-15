d1cbc7e58ed81c96ff1edfbae945a455f78d1bcf
NatalieGichuki <nataliegichuki@gmail.com>
2025-05-15 13:40:03 +0300

    complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
+    file_path = f"{file_name}.txt"
+    with open(file_path, "w") as file:
+        file.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    file_path = f"{file_name}.txt"
+    with open(file_path, "a") as file:
+        file.write(append_content)
     pass
 
 def read_file(file_name):
+    file_path = f"{file_name}.txt"
+    with open(file_path, "r") as file:
+        return file.read().strip()
     pass
